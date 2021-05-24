ad295c812225a4b9bd42fa0166d3c7691cb86ce2
pohern <ohern_p@yahoo.com>
2021-05-24 13:50:04 -0500

    Add show function to the controllers

---
+++ controllers/breakfast.js
@@ -0,0 +1,10 @@
+//Import .json after created
+// const Breakfast = require('../../breakfast.json');
+
+module.exports = {
+  show,
+};
+
+function show(req, res) {
+  console.log(req);
+}
---
+++ controllers/dinner.js
@@ -0,0 +1,10 @@
+//Import .json after created
+// const Dinner = require('../../dinner.json');
+
+module.exports = {
+    show
+}
+
+function show(req,res){
+    console.log(req)
+}
